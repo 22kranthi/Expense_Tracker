be1b24c931d014e7c1398efb71873fbff7e84999
22kranthi <kranthisabavat17@gmail.com>
2025-06-27 16:18:09 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className='flex w-full h-14 bg-green-500 items-center justify-center'>
+            <div className='pl-5 font-semibold text-2xl flex-grow text-center'>
+                <p>Money Manager💸</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
